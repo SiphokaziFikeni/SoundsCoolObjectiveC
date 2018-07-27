7be184307cf9dca5fd948d21d5b5351773970497
Unknown <dumisile.fikeni@gmail.com>
2018-07-27 12:49:32 +0200

    Initial project files

---
+++ SoundsCoolObjectiveC/SoundsCoolObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoundsCoolObjectiveC
+//
+//  Created by Sphokazi Fikeni on 2018/07/26.
+//  Copyright © 2018 Sphokazi Fikeni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SoundsCoolObjectiveC/SoundsCoolObjectiveC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SoundsCoolObjectiveC
+//
+//  Created by Sphokazi Fikeni on 2018/07/26.
+//  Copyright © 2018 Sphokazi Fikeni. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
