ee007c64d1ff9e4b88d917f9acf4542a50ad17ea
Unknown <dumisile.fikeni@gmail.com>
2018-07-30 08:00:32 +0200

    Dummy data

--- SoundsCoolObjectiveC/SoundsCoolObjectiveC/ViewController.h
+++ SoundsCoolObjectiveC/SoundsCoolObjectiveC/ViewController.h
@@ -7,9 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
+{
 
-
+    IBOutlet UITableView *songsTableView;
+    NSMutableArray *songArray;
+}
 @end
 
--- SoundsCoolObjectiveC/SoundsCoolObjectiveC/ViewController.m
+++ SoundsCoolObjectiveC/SoundsCoolObjectiveC/ViewController.m
@@ -16,13 +16,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"This is some data written in a log.");
+    
+    songArray = [[NSMutableArray alloc]initWithObjects: @"New Delhi", @"Joburg", @"Durban", @"Cape Town", @"Mthatha", @"Sydney", nil];
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    
+    static NSString *cellIdentfier = @"SongsTableViewCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentfier forIndexPath:indexPath];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentfier];
+    }
+    
+    [cell.textLabel setText:[songArray objectAtIndex: indexPath.row]];
+    
+    return cell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [songArray count];
 }
 
 
