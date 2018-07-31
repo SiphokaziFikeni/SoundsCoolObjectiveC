cd69f1094221984176de8bc55845254c8952b633
Unknown <dumisile.fikeni@gmail.com>
2018-07-31 16:44:38 +0200

    SongsViewModel implementation

--- SoundsCoolObjectiveC/SoundsCoolObjectiveC/ViewModel/SongsViewModel.h
+++ SoundsCoolObjectiveC/SoundsCoolObjectiveC/ViewModel/SongsViewModel.h
@@ -7,7 +7,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "SongsRepository.h"
+#import <UIKit/UIKit.h>
 
 @interface SongsViewModel : NSObject
+
+@property SongsRepository* songsRepo;
+@property NSMutableArray<Song*>* songsList;
+
+-(id)init;
+-(NSUInteger)amountOfSongs;
+-(NSString*)formattedAlbumTextAt:(NSInteger)index;
+-(NSString*)songTitleTextAt:(NSInteger)index;
+-(NSString*)artistTextAt:(NSInteger)index;
+-(UIImage*)albumImageAt:(NSInteger)index;
 
 @end
--- SoundsCoolObjectiveC/SoundsCoolObjectiveC/ViewModel/SongsViewModel.m
+++ SoundsCoolObjectiveC/SoundsCoolObjectiveC/ViewModel/SongsViewModel.m
@@ -10,4 +10,33 @@
 
 @implementation SongsViewModel
 
+-(id)init {
+    self = [super init];
+    self.songsRepo = [[SongsRepository alloc] init];
+    self.songsList = self.songsRepo.getAllSongs;
+    return self;
+}
+
+-(NSUInteger)amountOfSongs {
+    return self.songsRepo.songsList.count;
+}
+
+-(NSString*)formattedAlbumTextAt:(NSInteger)index {
+    NSString* albumName = [[self.songsList objectAtIndex:index] album];
+    NSString* albumYear = [[self.songsList objectAtIndex:index] releaseYear];
+    return [NSString stringWithFormat:@"%@ (%@)", albumName, albumYear];
+}
+
+-(NSString*)songTitleTextAt:(NSInteger)index {
+    return [[self.songsList objectAtIndex:index] title];
+}
+
+-(NSString*)artistTextAt:(NSInteger)index {
+    return [[self.songsList objectAtIndex:index] artist];
+}
+
+-(UIImage*)albumImageAt:(NSInteger)index {
+    return [[self.songsList objectAtIndex:index] albumArt];
+}
+
 @end
